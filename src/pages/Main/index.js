b13bc31ef06b1bbacba8dca6b6054ba68f6c107c
Diego Rodrigues <diegurm@hotmail.com>
2020-03-14 01:07:54 -0300

    feat - Acessando Api do Github

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import api from '../../services/api';
 import {Container, Form, Input, SubmitButton} from './styles';
+import { Keyboard } from 'react-native';
 
 export default function Main() {
+  const [newUser, setNewUser] = useState();
+  const [user, setUsers] = useState();
+
+  handleAddUser = async () => {
+    const response = await api.get(`/users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+
+    setUsers([...users, data]);
+    setNewUser('');
+
+    Keyboard.dismiss();
+  };
+
   return (
     <Container>
       <Form>
@@ -10,9 +32,13 @@ export default function Main() {
           autoCorret={false}
           autoCapitalize="none"
           placeholder="Adicionar usuário"
+          value={newUser}
+          onChangeText={text => setNewUser(text)}
+          returnKeyType="send"
+          onSubmitEditing={handleAddUser}
         />
 
-        <SubmitButton>
+        <SubmitButton onPress={handleAddUser}>
           <Icon name="add" size={20} color="#FFF" />
         </SubmitButton>
       </Form>
